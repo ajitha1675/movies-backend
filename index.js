8362751185e69c41c838e84e6db31dc69c402ce4
Ajilalitha <ajilalitha010@gmail.com>
2024-10-17 12:47:40 +0530

    add the readme

--- index.js
+++ index.js
@@ -4,9 +4,31 @@ const app =  express();
 const port = 2000;
 
 app.get('/', (req,res ) => {
-    res.json({msg:"hello the viewers"})
+    res.json({msg:"Hello viewers"})
 })
 
+//CRUD o/p
+
+//r- reading
+app.get('/movies',()=>{
+
+})
+
+//c-create
+app.post('/movies',()=>{
+
+})
+
+//u-update
+app.put('/movies/:id', ()=>{
+
+})
+
+//d-delete
+
+app.delete('/movies/id', ()=>{
+    
+})
 app.listen(2000, ()=>{
     console.log(`The server is running at http://localhost:${port}`);
     
