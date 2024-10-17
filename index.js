027be4bf873d15a1deffe08597de40d52495d447
Ajilalitha <ajilalitha010@gmail.com>
2024-10-17 15:01:21 +0530

    Add the route file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import movieroute from './routes/movie.route.js';
 
 const app =  express();
 const port = 2000;
@@ -7,28 +8,9 @@ app.get('/', (req,res ) => {
     res.json({msg:"Hello viewers"})
 })
 
-//CRUD o/p
+app.use('/movies', movieroute);
 
-//r- reading
-app.get('/movies',()=>{
 
-})
-
-//c-create
-app.post('/movies',()=>{
-
-})
-
-//u-update
-app.put('/movies/:id', ()=>{
-
-})
-
-//d-delete
-
-app.delete('/movies/id', ()=>{
-    
-})
 app.listen(2000, ()=>{
     console.log(`The server is running at http://localhost:${port}`);
     
---
+++ routes/movie.route.js
@@ -0,0 +1,25 @@
+//CRUD o/p
+import express from "express";
+import {movieIndex,  
+    movieCreate, 
+    movieUpdate,
+    movieDelete} from 
+    "../controllers/movie.controller";
+
+
+const router = express.Router();
+
+//r- reading
+router.get("/",movieIndex)
+
+//c-create
+router.post('/',movieCreate)
+
+//u-update
+router.put('/:id',movieUpdate)
+
+//d-delete 
+
+router.delete('/:id', movieDelete) 
+
+export default router;
